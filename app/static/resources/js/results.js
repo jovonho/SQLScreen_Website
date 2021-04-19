eacabcac0d6e7f42e986f4c47b06eb17316f5f66
Loic Ho-Von <loic.hovon@gmail.com>
2021-04-19 16:10:15 -0400

    Excel file download working

--- app/static/resources/js/results.js
+++ app/static/resources/js/results.js
@@ -1,38 +1,38 @@
-function resizeNames() {
 
-    names = $('h3.name');
+function downloadResults(e) {
+    e.stopPropagation();
+    e.preventDefault();
 
-    $.each(names, (i, obj) => {
-        if ($(obj).text().length > 125)
-            $(obj).css('font-size', '100%')
-    })
+    const query = document.querySelector("#query").dataset.query;
+    const sortby = document.querySelector('#sort-by').value;
+    const sortorder = document.querySelector('#sort-order').getAttribute('value');
 
-}
+    const request = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "query": query,
+            "sortby": sortby,
+            "sortorder": sortorder
+        })
+    };
 
-function collapseResult(elem) {
+    const filename = 'query_result_' + Date.now() + '.csv';
 
-    $(elem).find('.line-info').slideToggle(100, function () {
-        if ($(this).is(':visible'))
-            $(this).css('display', 'flex');
-    });
-
-    $(elem).find('.hidden').slideToggle(100, function () {
-        if ($(this).is(':visible'))
-            $(this).css('display', 'flex');
-    });
-}
-
-function openResult(elem) {
-
-    $(elem).find('.line-info').slideToggle(100, function () {
-        if (!$(this).is(':visible'))
-            $(this).css('display', 'none');
-    });
-
-    $(elem).find('.hidden').slideToggle(100, function () {
-        if (!$(this).is(':visible'))
-            $(this).css('display', 'none');
-    });
+    fetch("/exportcsv", request)
+        .then(resp => resp.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        });
 }
 
 
@@ -40,40 +40,7 @@ $(document).ready(function () {
 
     resizeNames();
 
-    $('#download-button').on("click", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-
-        const query = document.querySelector("#query").dataset.query;
-        const sortby = document.querySelector('#sort-by').value;
-        const sortorder = document.querySelector('#sort-order').getAttribute('value');
-
-        const request = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "query": query,
-                "sortby": sortby,
-                "sortorder": sortorder
-            })
-        };
-
-        const filename = 'query_result_' + Date.now() + '.csv'
-        fetch("/exportcsv", request)
-            .then(resp => resp.blob())
-            .then(blob => {
-                const url = window.URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.style.display = 'none';
-                a.href = url;
-                a.download = filename;
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-            });
-    })
+    $('#download-button').on("click", (e) => downloadResults(e))
 
     $('.result__info').on("click", (e) => {
         console.log(e.currentTarget + " clicked")
@@ -112,7 +79,42 @@ $(document).ready(function () {
 
             $(e.currentTarget).text("Collapse All")
         }
+    });
+});
 
+function resizeNames() {
+
+    names = $('h3.name');
+
+    $.each(names, (i, obj) => {
+        if ($(obj).text().length > 125)
+            $(obj).css('font-size', '100%')
+    })
+
+}
+
+function collapseResult(elem) {
+
+    $(elem).find('.line-info').slideToggle(100, function () {
+        if ($(this).is(':visible'))
+            $(this).css('display', 'flex');
     });
 
-});
+    $(elem).find('.hidden').slideToggle(100, function () {
+        if ($(this).is(':visible'))
+            $(this).css('display', 'flex');
+    });
+}
+
+function openResult(elem) {
+
+    $(elem).find('.line-info').slideToggle(100, function () {
+        if (!$(this).is(':visible'))
+            $(this).css('display', 'none');
+    });
+
+    $(elem).find('.hidden').slideToggle(100, function () {
+        if (!$(this).is(':visible'))
+            $(this).css('display', 'none');
+    });
+}
